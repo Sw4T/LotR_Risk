1721de21c143367fbc9bda17072b9924af34c5c3
Sw4T <pierre.gambier@yahoo.fr>
2014-11-04 23:27:18 +0100

    Test pour ajouter/supprimer territoire aux régions et joueurs

--- LOTR_Risk/src/tests/Test_Data.java
+++ LOTR_Risk/src/tests/Test_Data.java
@@ -1,6 +1,7 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import objects.Joueur;
 import objects.Region;
 import objects.Territoire;
 
@@ -8,29 +9,44 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import utils.LOTR_Data;
-
 public class Test_Data {
 
 	//private LOTR_Data data;
 	private Region region;
 	private Territoire territoire;
+	private Joueur joueur;
 	
 	@Before
 	public void setUp() throws Exception {
 		region = new Region("testRegion", 5);
 		territoire = new Territoire("testTerritoire", false);
+		joueur = new Joueur("Arthure");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		region = null;
+		joueur = null;
 		territoire = null;
 	}
 
 	@Test
-	public void test_add_simple_territoire() {
+	public void test_add_region_territoire() {
 		region.add_Territoire(territoire);
+		assertEquals(region.get_Territoire("testTerritoire"), territoire);
+	}
+	
+	@Test
+	public void test_add_joueur_territoire() {
+		joueur.add_Territoire(territoire);
+		assertEquals(joueur.has_Territoire(territoire), true);
+	}
+	
+	@Test
+	public void test_remove_joueur_territoire() {
+		joueur.add_Territoire(territoire);
+		joueur.remove_Territoire(territoire);
+		assertEquals(joueur.has_Territoire(territoire), false);
 	}
 
 }
