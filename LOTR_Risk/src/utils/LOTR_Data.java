bfe025224e4b952ecb37fbeab05855afc1afcda0
Sw4T <pierre.gambier@yahoo.fr>
2014-11-09 01:52:35 +0100

    Ajout des fonctions playerHasRegion, getListTerritoireFromType

--- LOTR_Risk/src/utils/LOTR_Data.java
+++ LOTR_Risk/src/utils/LOTR_Data.java
@@ -2,9 +2,16 @@ package utils;
 
 import java.util.ArrayList;
 
+import objects.Joueur;
 import objects.Region;
 import objects.Territoire;
+import objects.TypeTerritoire;
 
+/**
+ * Classe regroupant les données et les méthodes servant à utiliser la plateau <br>
+ * <b> ATTENTION A LA CASSE ET AUX ACCENTS HEIN !! </b>
+ * @author Swatosj
+ */
 public class LOTR_Data {
 	
 	private ArrayList<Region> mapRegion;
@@ -30,81 +37,105 @@ public class LOTR_Data {
 		return null;
 	}
 	
+	public ArrayList<Territoire> getListTerritoireFromType(TypeTerritoire type) {
+		ArrayList<Territoire> toReturn = new ArrayList<Territoire>();
+		for (Region r : this.mapRegion) {
+			for (Territoire t : r.getListTerritoire()) {
+				if (t.getType_T() == type) {
+					toReturn.add(t);
+				}
+			}
+		}
+		return toReturn;
+	}
+	
+	public boolean playerHasRegion(Joueur j, String nomRegion) {
+		Region r = getRegionByName(nomRegion);
+		if (r != null) {
+			for (Territoire t : r.getListTerritoire()) {
+				if (!j.getListTerritoire().contains(t))
+					return false;
+			}
+		} else
+			return false;
+		return true;
+	}
+	
 	private Region init_list_Territoire_Rohan()
 	{
 		Region r = new Region("Rohan", 4);
-		r.add_Territoire(new Territoire("Pays des Vieux Biscornus", true));
-		r.add_Territoire(new Territoire("Trouée du Rohan", false));
-		r.add_Territoire(new Territoire("Fangorn", true));
-		r.add_Territoire(new Territoire("Enedwaith", false));
-		r.add_Territoire(new Territoire("Miniriath", false));
-		r.add_Territoire(new Territoire("Pays de Dun", false));
-		r.add_Territoire(new Territoire("Eregion", false));
+		r.add_Territoire(new Territoire("Pays des Vieux Biscornus", true, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Trouée du Rohan", false, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Fangorn", true, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Enedwaith", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Miniriath", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Pays de Dun", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Eregion", false, TypeTerritoire.NEUTRE));
 		return r;
 	}
 	
 	private Region init_list_Territoire_Rhovanion()
 	{
 		Region r = new Region("Rhovanion", 5);
-		r.add_Territoire(new Territoire("Marais des Morts", false));
-		r.add_Territoire(new Territoire("Le Plateau", false));
-		r.add_Territoire(new Territoire("Terres Brunes", false));
-		r.add_Territoire(new Territoire("Collines du Rhun", false));
-		r.add_Territoire(new Territoire("Lorien", false));
-		r.add_Territoire(new Territoire("Emyn Muil", false));
-		r.add_Territoire(new Territoire("Moria", true));
-		r.add_Territoire(new Territoire("Champs aux Iris", false));
+		r.add_Territoire(new Territoire("Marais des Morts", false, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Le Plateau", false, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Terres Brunes", false, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Collines de Rhûn", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Lorien", false, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Emyn Muil", false, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Moria", true, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Champs aux Iris", false, TypeTerritoire.NEUTRE));
 		return r;
 	}
 	
 	private Region init_list_Territoire_Eriador()
 	{
 		Region r = new Region("Eriador", 3);
-		r.add_Territoire(new Territoire("Collines des Tours", false));
-		r.add_Territoire(new Territoire("La Comté", false));
-		r.add_Territoire(new Territoire("Les Collines d'Evendim", true));
-		r.add_Territoire(new Territoire("Harlindon", false));
-		r.add_Territoire(new Territoire("Forlindon", false));
-		r.add_Territoire(new Territoire("Montagnes Bleues", false));
-		r.add_Territoire(new Territoire("Mithlond", false));
+		r.add_Territoire(new Territoire("Collines des Tours", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("La Comté", false, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Les Collines d'Evendim", true, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Harlindon", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Forlindon", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Montagnes Bleues", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Mithlond", false, TypeTerritoire.NEUTRE));
 		return r;
 	}
 	
 	private Region init_list_Territoire_Arnor()
 	{
 		Region r = new Region("Arnor", 7);
-		r.add_Territoire(new Territoire("Pays de Bouc", false));
-		r.add_Territoire(new Territoire("Vieille Forêt", false));
-		r.add_Territoire(new Territoire("Rhudaur", true));
-		r.add_Territoire(new Territoire("Fornost", false));
-		r.add_Territoire(new Territoire("Hauts du Nord", false));
-		r.add_Territoire(new Territoire("Hauts du Sud", false));
-		r.add_Territoire(new Territoire("Collines du Temps", false));
-		r.add_Territoire(new Territoire("Landes d'Etten", false));
-		r.add_Territoire(new Territoire("Angmar", false));
-		r.add_Territoire(new Territoire("Est d'Angmar", false));
-		r.add_Territoire(new Territoire("Forodwaith", false));
+		r.add_Territoire(new Territoire("Pays de Bouc", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Vieille Forêt", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Rhudaur", true, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Fornost", false, TypeTerritoire.BIEN));
+		r.add_Territoire(new Territoire("Hauts du Nord", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Hauts du Sud", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Collines du Temps", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Landes d'Etten", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Angmar", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Est d'Angmar", false, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Forodwaith", false, TypeTerritoire.NEUTRE));
 		return r;
 	}
 	
 	private Region init_list_Territoire_Foret_Noire()
 	{
 		Region r = new Region("Forêt Noire", 4);
-		r.add_Territoire(new Territoire("Carrock", false));
-		r.add_Territoire(new Territoire("Vallée de l'Anduin", false));
-		r.add_Territoire(new Territoire("Sud de la Forêt Noire", true));
-		r.add_Territoire(new Territoire("Est de la Forêt Noire", false));
-		r.add_Territoire(new Territoire("Nord de la Forêt Noire", false));
+		r.add_Territoire(new Territoire("Carrock", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Vallée de l'Anduin", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Sud de la Forêt Noire", true, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Est de la Forêt Noire", false, TypeTerritoire.MAL));
+		r.add_Territoire(new Territoire("Nord de la Forêt Noire", false, TypeTerritoire.BIEN));
 		return r;
 	}
 	
 	private Region init_list_Territoire_Rhun()
 	{
 		Region r = new Region("Rhûn", 2);
-		r.add_Territoire(new Territoire("Esgaroth", false));
-		r.add_Territoire(new Territoire("Rhûn du Sud", false));
-		r.add_Territoire(new Territoire("Rhûn du Nord", false));
-		r.add_Territoire(new Territoire("Landes Déséchée", false));
+		r.add_Territoire(new Territoire("Esgaroth", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Rhûn du Sud", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Rhûn du Nord", false, TypeTerritoire.NEUTRE));
+		r.add_Territoire(new Territoire("Landes Desséchée", false, TypeTerritoire.MAL));
 		return r;
 	}
 }
