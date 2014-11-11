86790f0dcc2310a7f10f73bb2b442651d512bb5b
Pierrot <pierre.gambier@yahoo.fr>
2014-11-11 23:31:28 +0100

    Ajout de l'envoi dynamique de plusieurs joueurs

--- LOTR_Risk/src/net/TestClient.java
+++ LOTR_Risk/src/net/TestClient.java
@@ -2,7 +2,6 @@ package net;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -21,18 +20,21 @@ public class TestClient {
 			s.setSoTimeout(4000);
 			Scanner input = new Scanner(System.in);
 			ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
-			Joueur j = new Joueur("SEIKOMI");
 			String entree = input.nextLine();
+			String entreeJoueur;
 			do
 			{
 				if (entree != null && !entree.equals("")) {
-					oos.writeObject((Object) entree);
-					oos.flush();
+					envoyerObject(oos, entree);
 				}
 				if (entree != null && entree.equals("obj")) 
 				{
-					oos.writeObject(j);
-					oos.flush();
+					Integer nbJoueurs = Integer.parseInt(input.nextLine());
+					envoyerObject(oos, nbJoueurs);
+					for (int i = 0; i < nbJoueurs; i++) {
+						entreeJoueur = input.nextLine();
+						envoyerObject(oos, new Joueur(entreeJoueur));
+					}
 				}
 			} while (!(entree = input.nextLine()).equals("tg"));
 			input.close();
@@ -41,6 +43,11 @@ public class TestClient {
 		} catch (IOException e) { e.printStackTrace(); 
 		} 
 		
+	}
+	
+	public static synchronized void envoyerObject(ObjectOutputStream oos, Object o) throws IOException {
+		oos.writeObject(o);
+		oos.flush();
 	}
 
 }
