16d33a18a528ecaf31e10461d967ae2fb820f6f1
Pierrot <pierre.gambier@yahoo.fr>
2014-11-11 20:12:36 +0100

    Initial Commit

---
+++ LOTR_Risk_Android/src/com/lotr_risk/StartUpActivity.java
@@ -0,0 +1,13 @@
+package com.lotr_risk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_up);
+	}
+}
