05b7b5720b207e21cafc8963cf2220899f05523b
Sw4T <pierre.gambier@yahoo.fr>
2014-11-14 03:04:58 +0100

    Conception modifiée, changement du traitement

--- LOTR_Risk/src/net/ThreadConnexion.java
+++ LOTR_Risk/src/net/ThreadConnexion.java
@@ -5,38 +5,43 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ThreadConnexion implements Runnable {
+public class ThreadConnexion extends Thread {
 
 	private ServerSocket serveur;
 	private ThreadEnvoiReception T_DATA;
-	protected static AtomicBoolean hasFinished; //Boolean pouvant �tre modifi� dans n'importe quelle classe li�e au package net
+	protected AtomicBoolean hasFinished; //Boolean pouvant �tre modifi� dans n'importe quelle classe li�e au package net
 	
 	public ThreadConnexion() throws IOException {
+		super();
 		this.serveur = new ServerSocket(9876);
-		ThreadConnexion.hasFinished = new AtomicBoolean(false); 
+		this.hasFinished = new AtomicBoolean(false); 
 	}
 	
 	@Override
 	public void run() {
 		Socket socketEntree = null;
-		while (!ThreadConnexion.hasFinished.get()) //V�rifie si une connexion est achev�e ou non
+		while (!this.hasFinished.get()) //V�rifie si une connexion est achev�e ou non
 		{
 			try {
 				socketEntree = this.serveur.accept();
 				System.out.println("Un client avec l'adresse " + socketEntree.getInetAddress().getHostAddress() + " se connecte..");
 				System.out.println("Serveur : Allo j'�coute ?");
 				this.T_DATA = new ThreadEnvoiReception(socketEntree);
-				new Thread(this.T_DATA).start(); //Lancement du Thread échange de données
 			} catch (IOException e) {e.printStackTrace();}
 		}
 		try {
 			this.serveur.close();
+			System.out.println("Arrêt du serveur...");
 		} catch (IOException e) {e.printStackTrace();}
 	}
 	
-	/*public Object[] getInfosJoueurs() {
-		return this.T_DATA.getInfosJoueurs();
-	}*/
+	public void close() {
+		this.hasFinished.set(true);
+	}
+	
+	public ThreadEnvoiReception getThreadDonnees() {
+		return this.T_DATA;
+	}
 
 	
 }
