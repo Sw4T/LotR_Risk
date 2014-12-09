97bdad19c8aaa4c77700aab5cb059df2e11cdd76
Sw4T <pierre.gambier@yahoo.fr>
2014-12-09 15:03:25 +0100

    Ajout de la réception de joueurs

--- LOTR_Risk_Android/src/com/game/InterfaceLOTR.java
+++ LOTR_Risk_Android/src/com/game/InterfaceLOTR.java
@@ -5,7 +5,8 @@ package com.game;
  * @author pierrot
  */
 public interface InterfaceLOTR {
-
+	
+	public final static int RECEPTION_JOUEURS = 2;
 	public final static int ENVOI_JOUEURS = 1;
 	public final static int FERMER_SERVEUR = 0;
 }
