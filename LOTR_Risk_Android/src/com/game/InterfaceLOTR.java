0d3165272ab7c67785d1cc8237cfd5be0b085d4f
Sw4T <pierre.gambier@yahoo.fr>
2014-12-09 15:16:42 +0100

    Ajout de la procédure d'envoi des joueurs par le serveur, changement des noms pour plus de clarté

--- LOTR_Risk_Android/src/com/game/InterfaceLOTR.java
+++ LOTR_Risk_Android/src/com/game/InterfaceLOTR.java
@@ -6,7 +6,8 @@ package com.game;
  */
 public interface InterfaceLOTR {
 	
-	public final static int RECEPTION_JOUEURS = 2;
-	public final static int ENVOI_JOUEURS = 1;
+	public final static int SERVEUR_RECEPTION_JOUEURS = 3;
+	public final static int SERVEUR_ENVOI_JOUEURS = 2;
+	public final static int CREATION_JOUEURS = 1;
 	public final static int FERMER_SERVEUR = 0;
 }
