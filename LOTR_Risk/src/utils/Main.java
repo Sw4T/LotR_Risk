b3b517a317d1ae9e531a45c9a322f74641755d88
Sw4T <pierre.gambier@yahoo.fr>
2014-12-13 19:05:26 +0100

    Ajout du traitement envoyant la liste des joueurs à l'application, gestion des erreurs de connexion

--- LOTR_Risk/src/utils/Main.java
+++ LOTR_Risk/src/utils/Main.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.io.EOFException;
+import java.net.SocketException;
 
 public class Main implements InterfaceLOTR {
 
@@ -16,32 +17,40 @@ public class Main implements InterfaceLOTR {
 			Integer constanteTraitement;
 			jeu.attenteConnexionClient();
 			do {
-				constanteTraitement = jeu.recupererTraitement(); //Récupère l'entier définissant une constante
-				if (constanteTraitement != null) 
+				try 
 				{
-					try 
+					constanteTraitement = jeu.recupererTraitement(); //Récupère l'entier définissant une constante
+					if (constanteTraitement != null)  
 					{
 						switch (constanteTraitement.intValue()) 
 						{
-							case PROCEDURE_JOUEURS :
+							case CREATION_JOUEURS :
 								if (jeu.init_joueurs_territoires()) { //Retourne true si la procédure a bien aboutie
-									for (int i = 0; i < jeu.getTabJoueurs().size(); i++) {
-										System.out.println(jeu.getTabJoueurs().get(i)); //Affichage des territoires de chaque joueur
+									for (int i = 0; i < jeu.getJoueurs().size(); i++) {
+										System.out.println(jeu.getJoueurs().get(i)); //Affichage des territoires de chaque joueur
 									}
 								} else
 									System.out.println("Erreur lors de l'initialisation des joueurs");
+								break;
+							case SERVEUR_ENVOI_JOUEURS :
+								System.out.println("Envoi des joueurs à l'application distante...");
+								jeu.sendJoueurs_ToRemote(); //Gestion exceptions TODO
 								break;
 							case FERMER_SERVEUR : 
 								jeu.getThreadConnexion().close(); //Ferme le serveur
 								break;
 							default : System.out.println("Constante non implémentée ou inconnue");
 						}
-					}
-					catch (EOFException ex) { //Cas typique d'une fermeture de socket
-						System.out.println("Erreur de connexion, fermeture de celle-ci...");
-					}
-				} else
-					System.out.println("Format de données invalide");
+					} 
+					else
+						System.out.println("Format de données invalide");
+				}
+				catch (EOFException | SocketException sock) { //Fermeture de la connexion cliente en cas d'exception levée
+					System.out.println("Erreur lors de la communication cliente, fermeture de la connexion...");
+					jeu.fermerThreadClient();
+					jeu.getThreadConnexion().setThreadDonnees(null);
+					jeu.attenteConnexionClient();
+				}
 			} while (!jeu.getThreadConnexion().isServeurFinished()); 
 		} 
 		catch (Throwable e) {
