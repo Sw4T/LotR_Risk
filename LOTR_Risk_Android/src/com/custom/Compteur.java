19a67c5846f654dfe86889500db791224beea646
Sw4T <pierre.gambier@yahoo.fr>
2014-12-13 18:17:10 +0100

    Ajout d'un compteur customisé

---
+++ LOTR_Risk_Android/src/com/custom/Compteur.java
@@ -0,0 +1,53 @@
+package com.custom;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.lotr_risk.R;
+
+public class Compteur extends View {
+
+	private Button BT_Plus, BT_Moins;
+	private EditText compteur;
+	private View vue;
+	private int nbCompteur = 0;
+	
+	public Compteur(Context context, View vueCompteur) {
+		super(context); 
+		this.vue = vueCompteur;
+		inflateElements();
+	}
+	
+	public void inflateElements() {
+		BT_Plus = (Button) vue.findViewById(R.id.BT_Plus);
+		BT_Moins = (Button) vue.findViewById(R.id.BT_Moins);
+		compteur = (EditText) vue.findViewById(R.id.compteur);
+		compteur.setClickable(false);
+		BT_Plus.setOnClickListener(BT_Plus_Listener);
+		BT_Moins.setOnClickListener(BT_Moins_Listener);
+	}
+	
+	public Integer getNombreCompteur() {
+		return (Integer.parseInt(compteur.getText().toString()));
+	}
+
+	private OnClickListener BT_Plus_Listener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			nbCompteur++;
+			compteur.setText(String.valueOf(nbCompteur));
+		}
+	};
+	
+	private OnClickListener BT_Moins_Listener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if (nbCompteur > 0)
+				nbCompteur--;
+			compteur.setText(String.valueOf(nbCompteur));
+		}
+	};
+
+}
