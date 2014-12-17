bcdd6882f58ad8e00593648cbbe75a7cae2c029c
Sw4T <pierre.gambier@yahoo.fr>
2014-12-17 16:29:01 +0100

    Ajout de la procédure de réception de joueurs pour le serveur (SERVEUR_RECEPTION_JOUEURS)

--- LOTR_Risk_Android/src/com/net/TacheTransmission.java
+++ LOTR_Risk_Android/src/com/net/TacheTransmission.java
@@ -4,26 +4,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import objects.Joueur;
-import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.game.InterfaceLOTR;
 
 public class TacheTransmission extends AsyncTask<Integer, Void, ArrayList<Joueur>> implements InterfaceLOTR {
 
-	//private Context context;
 	private DonneesConnexion connexion;
 	private ArrayList<Joueur> listJoueurs;
 	
-	public TacheTransmission(Context context, DonneesConnexion con, ArrayList<Joueur> listJoueur) 
+	public TacheTransmission(DonneesConnexion con, ArrayList<Joueur> listJoueur) 
 	{
-		//this.context = context;
 		this.connexion = con;
 		this.listJoueurs = listJoueur;
 	}
 	
 	@Override
 	protected ArrayList<Joueur> doInBackground(Integer ... params) {
+		if (this.connexion == null)
+			return null;
 		Integer traitement = params[0];
 		Integer nbJoueurs = listJoueurs.size();
 		switch (traitement.intValue()) 
@@ -51,9 +51,30 @@ public class TacheTransmission extends AsyncTask<Integer, Void, ArrayList<Joueur
 					this.connexion.getOutput().sendInt(SERVEUR_ENVOI_JOUEURS);
 					if (!(this.connexion.getString().equals("#OK"))) //Vérification de la synchro du serveur
 						return null;
+					if (nbJoueurs == 2) { //Gestion du neutre
+						nbJoueurs++;
+						listJoueurs.add(new Joueur("Neutre", "#ff000000"));
+					}
 					for (int i = 0; i < nbJoueurs; i++) {
 						Joueur joueurRecu = connexion.getInput().getJoueur();
 						listJoueurs.set(i, joueurRecu);
+					}
+					return listJoueurs;
+				} 
+				catch (IOException | ClassNotFoundException e) {
+					e.printStackTrace();
+					return null;
+				}
+			case SERVEUR_RECEPTION_JOUEURS :
+				try 
+				{
+					this.connexion.getOutput().sendInt(SERVEUR_RECEPTION_JOUEURS);
+					if (!(this.connexion.getString().equals("#OK"))) //Vérification de la synchro du serveur
+						return null;
+					for (int i = 0; i < nbJoueurs; i++) {
+						Joueur toSend = listJoueurs.get(i);
+						Log.d("kiki", toSend.toString());
+						connexion.getOutput().sendJoueur(toSend);
 					}
 					return listJoueurs;
 				} 
